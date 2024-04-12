6efcf1752c69cdb33ec6ff36234b28f046932333
Satyadeep Mishra <satstardom@gmail.com>
2024-04-13 03:22:41 +0530

    Added files

---
+++ src/constants/index.js
@@ -0,0 +1,76 @@
+import project1 from "../assets/projects/event.jpg";
+import project2 from "../assets/projects/fitness.jpg";
+import project3 from "../assets/projects/recipe.jpg";
+import project4 from "../assets/projects/online.jpg";
+
+export const HERO_CONTENT = `I'm a dedicated full stack developer with a passion for building reliable and scalable web applications. Over the past 5 years, I've gained extensive experience in front-end technologies such as React and Next.js, as well as back-end technologies including Node.js, MySQL, PostgreSQL, and MongoDB. My objective is to utilize my skills to develop innovative solutions that propel business expansion and provide outstanding user experiences.`;
+
+export const ABOUT_TEXT = `I'm an enthusiastic and adaptable full stack developer passionate about crafting effective and intuitive web applications. With 5 years of industry experience, I've mastered various technologies like React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development sparked from a deep curiosity about functionality, evolving into a career where I thrive on learning and embracing new obstacles. I excel in teamwork, relishing in solving intricate challenges to deliver top-notch solutions. Beyond coding, I stay active, explore fresh technologies, and contribute to open-source initiatives.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2023 - Present",
+    role: "Senior Full Stack Developer",
+    company: "Microsoft Corp.",
+    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with PostgreSQL databases. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: ["Javascript", "React.js", "Vue.js", "mongoDB"],
+  },
+  {
+    year: "2022 - 2023",
+    role: "Frontend Developer",
+    company: "Amazon",
+    description: `Designed and developed user interfaces for web applications using Vue.js and React. Collaborated closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+    technologies: ["HTML", "CSS", "Angular.js", "mySQL"],
+  },
+  {
+    year: "2021 - 2022",
+    role: "Full Stack Developer",
+    company: "Apple Inc.",
+    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Worked with cross-functional teams to deliver high-quality software products on schedule.`,
+    technologies: ["Python", "Svelte", "Django", "Postgres"],
+  },
+  {
+    year: "2020 - 2021",
+    role: "Software Engineer",
+    company: "IBM",
+    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Collaborated with product managers to prioritize features and enhancements.`,
+    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Event Management System",
+    image: project1,
+    description:
+      "A comprehensive event management system designed to handle event planning, registration, and attendee management.",
+    technologies: ["HTML", "CSS", "Angular", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Fitness Tracking App",
+    image: project2,
+    description:
+      "An application for tracking fitness activities, setting goals, and monitoring progress over time.",
+    technologies: ["HTML", "CSS", "React", "Firebase"],
+  },
+  {
+    title: "Recipe Sharing Platform",
+    image: project3,
+    description:
+      "A platform for sharing and discovering recipes, with features such as recipe creation, rating, and commenting.",
+    technologies: ["HTML", "CSS", "Vue.js", "Bootstrap"],
+  },
+  {
+    title: "Online Learning Portal",
+    image: project4,
+    description:
+      "An online learning platform offering courses on various subjects, with features like video lectures, quizzes, and certificates.",
+    technologies: ["HTML", "CSS", "React", "Express", "Postgres"],
+  },
+];
+
+export const CONTACT = {
+  address: "Powder Gali, Gokuldham ",
+  phoneNo: "+91 10101 01010 ",
+  email: "syntaxspheres@gmail.com",
+};
